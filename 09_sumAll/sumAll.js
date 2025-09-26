146af0a6e3469d2c0009987ccd12ed0c3c6ea921
Dan J. Pasuengos <danjbuilds@users.noreply.github.com>
2025-09-26 18:24:27 +0800

    finished exercices 01 -09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
-};
+    // if (a > b){
+    //     let temp = a;
+    //     a = b;
+    //     b = temp;
+    // }
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+   
+
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+
+    let sum = 0;
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
+  
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
