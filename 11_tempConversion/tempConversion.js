9226adb19bf97ea87820fbbc7095f72def1559e2
Dan J. Pasuengos <danjbuilds@users.noreply.github.com>
2025-09-28 11:40:14 +0800

    finished/solved 06 - 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+
+const convertToCelsius = function(fahrenheit) {
+  let convertToC = ((fahrenheit - 32)*(5 / 9));
+  return Math.round(convertToC * 10) / 10
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertToF = (celsius * (9 / 5)) + 32;
+  return Math.round(convertToF * 10) / 10
+
+
 };
 
 // Do not edit below this line
